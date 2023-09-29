1293b220f4ff121dc79ce9299fd1119995f8ce11
VEER <birat.adak@gmail.com>
2023-09-29 18:47:33 +0530

    FT2023-442: Completed Todolist with delete feature

--- web/themes/custom/custom_theme/js/todo_list.js
+++ web/themes/custom/custom_theme/js/todo_list.js
@@ -1,98 +1,115 @@
-// localStorage.removeItem('todos');
 (function ($, Drupal) {
   Drupal.behaviors.todo_list = {
     // To Set Todo lists.
     attach: function (context, settings) {
       // Define the setting if not initialized.
       once("todoBehaviour", "#todo-list", context).forEach((element) => {
-        // Render Todos list.
-        if (getLocalStorage()) {
+        // Render Todos list if data is avialable.
+        if (getLocalStorage() && getLocalStorage().length) {
           renderTodos(getLocalStorage());
-        } else {
+        } 
+        else {
           $("#todo-list").html("No Todos Available");
         }
-
-        //tsting
-        // deleteTodo(3);
       });
 
-      // Unbinding the default behaviors.
-      $("#todo-submit").unbind("click");
-      // Adding todos on click.
+      // Initialize messanger object.
       const messenger = new Drupal.Message();
-      $("#todo-submit").click(function (e) {
+
+      // Adding todos on click.
+      $(".todo-action").unbind("click");
+      $(".todo-action").on("click", "#todo-submit", function (e) {
         e.preventDefault();
         inputTodo = $("#todo-text").val();
         if (inputTodo !== "") {
           addTodo(inputTodo);
           renderTodos();
-        } else {
+        } 
+        else {
           messenger.add("Todos cannot be empty", { type: "warning" });
         }
       });
 
+      // Deleting the todos on click.
       $(".todo-delete").unbind("click");
-      $(".todo-delete").click(function(){
+      $(".todo-list").on("click", ".todo-delete", function () {
         buttonId = $(this).attr("id");
-        console.log("todo deleted: "+buttonId);
+        console.log("todo deleted: " + buttonId);
         deleteTodo(buttonId);
         renderTodos();
       });
 
+      /**
+       * Get the todos as array.
+       *
+       * @returns JSON array of todos.
+       */
+      function getLocalStorage() {
+        return JSON.parse(localStorage.getItem("todos"));
+      }
+
+      /**
+       * Shows the todo lists.
+       */
+      function renderTodos() {
+        // Setting each elements ID and value as list.
+        listArr = getLocalStorage().map(
+          (e) =>
+            "<li id='" +
+            e[0] +
+            "' >" +
+            e[1] +
+            "<button class='todo-delete' id='" +
+            e[0] +
+            "'>X</button>" +
+            "</li>"
+        );
+        renderElement = listArr.toString().replace(/,/g, "");
+        $("#todo-list").html(renderElement);
+      }
+
+      /**
+       * Add string to the end of todo list.
+       *
+       * @param string value
+       *  stores the string value of todo.
+       */
       function addTodo(value) {
         // When local storage has values.
         console.log(getLocalStorage());
         if (getLocalStorage() && getLocalStorage().length) {
           oldTodos = getLocalStorage();
-          // Calculate last injected key
-          key= getLocalStorage()[getLocalStorage().length - 1][0] +1;
-          console.log("old data:",key);
-          oldTodos.push([key,value]);
+          // Calculate last injected todo id.
+          id = getLocalStorage()[getLocalStorage().length - 1][0] + 1;
+          console.log("old data:", id);
+          oldTodos.push([id, value]);
           localStorage.setItem("todos", JSON.stringify(oldTodos));
         }
         // When local storage is empty.
         else {
           var count = 0;
-          localStorage.setItem("todos", JSON.stringify([[count++,value]]));
+          localStorage.setItem("todos", JSON.stringify([[count++, value]]));
         }
       }
-      
-      // Get the todos as array 
-      function getLocalStorage() {
-        console.log(JSON.parse(localStorage.getItem("todos")));
-        return JSON.parse(localStorage.getItem("todos"));
-      }
 
-      // Show the todo lists.
-      function renderTodos() {
-        // Setting each elements ID and value as list.
-        listArr = getLocalStorage().map((e) => "<li id='"+e[0]+"' >" + e[1] + "<button class='todo-delete' id='"+e[0]+"'>X</button>"+"</li>");
-        renderElement = listArr.toString().replace(/,/g, "");
-        $("#todo-list").html(renderElement);
-      }
-
-      // Delete the todo from the list and HTML.
+      /**
+       * Delete the todo from the list and HTML.
+       *
+       * @param int id
+       * Stores the id of the todo.
+       */
       function deleteTodo(id) {
         // Check if local storage has values.
         if (getLocalStorage()) {
           oldTodos = getLocalStorage();
-          newTodos =[];
-          // Calculate last injected key
-          oldTodos.forEach(element => {
-            if(element[0]!=id){
-              console.log(element[0],element[0]!==id,id);
+          newTodos = [];
+          oldTodos.forEach((element) => {
+            // If the current element is not the clicked element add it to newArray.
+            if (element[0] != id) {
               newTodos.push(element);
             }
           });
-          // console.log("deleteing id: " + id,oldTodos,newTodos);
           localStorage.setItem("todos", JSON.stringify(newTodos));
-          $.ajax({
-            success:function(){
-              // $('.todo-list').html(getLocalStorage());
-              renderTodos();
-            }
-          });
-          // window.location.reload();
         }
       }
     },
